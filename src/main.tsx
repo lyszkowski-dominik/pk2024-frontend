9fdeccd35f9ca46c32e41a411b18387a9a8c7d1b
lyszkowski-dominik <57920532+lyszkowski-dominik@users.noreply.github.com>
2024-03-26 08:11:45 +0100

    react query and routing setup

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import { Provider } from "react-redux"
 import App from "./App"
 import { store } from "./app/store"
 import "./index.css"
+import { BrowserRouter } from "react-router-dom"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient()
 
 const container = document.getElementById("root")
 
@@ -12,13 +16,17 @@ if (container) {
 
   root.render(
     <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </React.StrictMode>,
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <Provider store={store}>
+            <App />
+          </Provider>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </React.StrictMode>
   )
 } else {
   throw new Error(
-    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
+    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file."
   )
 }
