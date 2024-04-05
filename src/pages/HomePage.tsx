90127a5494f884bb966712ce00ba4e80f513c961
lyszkowski-dominik <57920532+lyszkowski-dominik@users.noreply.github.com>
2024-04-05 20:47:52 +0200

    welcome message after login

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,12 @@
+import { useAppSelector } from "../app/hooks"
+import { selectUserData } from "../components/loginForm/loginFormSlice"
+
 const HomePage = () => {
+  const userData = useAppSelector(selectUserData)
   return (
     <div>
-      <h1>Home Page</h1>
+      {!userData && <h1>Home Page</h1>}
+      {userData && <h1>Witaj {userData.name} {userData.surname}</h1>}
     </div>
   )
 }
