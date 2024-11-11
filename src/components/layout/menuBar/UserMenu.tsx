26b35a60542ed45a5515df49377cd0d482fee70a
lyszkowski-dominik <57920532+lyszkowski-dominik@users.noreply.github.com>
2024-11-11 20:08:20 +0100

    Removed unnecessary refs for menu watching after switching to mui select. Changed a width to 100% on top menu for better clickability.

--- src/components/layout/menuBar/UserMenu.tsx
+++ src/components/layout/menuBar/UserMenu.tsx
@@ -1,77 +1,45 @@
-import { useState, useEffect, useRef } from 'react';
 import { useAppDispatch, useAppSelector } from '../../../app/hooks';
-import { useNavigate } from 'react-router';
 import { logOut } from '../../loginForm/loginFormSlice';
 import { useGetUserDataQuery } from '../../userProfile/userDataApiSlice';
 import { Link } from 'react-router-dom';
 import styles from './Menu.module.scss';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { selectSelectedCommunity } from '../../../features/communities/sharedDataSlice';
-import { Select, MenuItem } from '@mui/material';
+import { MenuItem, Select } from '@mui/material';
 
 const UserMenu = () => {
-  const [isOpen, setIsOpen] = useState(false);
   const dispatch = useAppDispatch();
-  const navigate = useNavigate();
   const {
-    data: detailedData,
-    isError,
-    isLoading,
-    isSuccess,
+    data: detailedData
   } = useGetUserDataQuery();
-  const menuRef = useRef<HTMLDivElement>(null);
   const communityId = useAppSelector(selectSelectedCommunity);
 
   const handleLogout = () => {
     dispatch(logOut());
-    setIsOpen(false);
     window.location.assign('/login');
   };
 
-  const handleClickOutside = (event: MouseEvent) => {
-    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
-      setIsOpen(false);
-    }
-  };
-
-  const handleMenuItemClick = () => {
-    setIsOpen(false);
-  };
-
-  useEffect(() => {
-    if (isOpen) {
-      document.addEventListener('mousedown', handleClickOutside);
-    } else {
-      document.removeEventListener('mousedown', handleClickOutside);
-    }
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, [isOpen]);
 
   return (
     <Select
-      sx={{height: '50px'}}
+      sx={{ height: '50px' }}
       value=""
       onChange={(event) => {
         const value = event.target.value;
-        if (value === 'profile') {
-          handleMenuItemClick();
-        } else if (value === 'logout') {
+        if (value === 'logout') {
           handleLogout();
         }
       }}
       displayEmpty
     >
-      <MenuItem value="" sx={{display:'none'}}>
+      <MenuItem value="" sx={{ display: 'none' }}>
         <div className={styles.menuItem}>
           <AccountCircleIcon />
           {detailedData?.first_name} {detailedData?.last_name}
         </div>
       </MenuItem>
       <MenuItem value="profile">
-        <Link to={`/hoa/${communityId}/user-profile`} onClick={handleMenuItemClick}>
+        <Link to={`/hoa/${communityId}/user-profile`}>
           Mój profil
         </Link>
       </MenuItem>
